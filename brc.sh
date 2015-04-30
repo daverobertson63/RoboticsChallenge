5e25896bc150bf76a8e3e4fd81ee7bfc6107ce1c
dave <daverobertson1963@gmail.com>
2015-04-30 14:29:33 +0100

    bug fixes

--- brc.sh
+++ brc.sh
@@ -105,6 +105,9 @@ echo "Copying all folders to sketches"
 mkdir sketches
 find RoboticsChallenge   -maxdepth 1 -mindepth 1 -type d -exec cp -r {} sketches \;
 rm -rf sketches/.git
+rm -rf Demos
+rm -rf Projects
+rm -rf Testing
 
 # Copy the latest script down to replace this one running
 cp "RoboticsChallenge/BRC Refresh.desktop" ~/Desktop
